fd0c7870b8d61ce5ff866afb509200e03b99a95b
Albert-Ros <albrol@mail.ru>
2020-04-24 02:23:13 +0300

    Добавлен метод birth date

--- CLASS Client Form.rb
+++ CLASS Client Form.rb
@@ -3,9 +3,13 @@ class Client_Form
   @pos1
   @pos2 
   @m_w
+  @data_born
+  @day
+  @month
+  @year
    def initialize()
     puts "Введите Ваше ФИО"
-    a=gets.capitalize
+    a=gets
   ar=a.split
   self.name=ar[0]
   self.sec_name=ar[1]
@@ -14,15 +18,15 @@ class Client_Form
    end
       def check_fio
         if (self.name.size<=45)&&(self.sec_name.size<=45)&&(self.last_name.size<=45)
-          puts "size check it\'s successful"
+          puts "size check is successful"
         else self.initialize()
           end
           if (self.name[/^\d+$/])||(self.sec_name[/^\d+$/])||(self.last_name[/^\d+$/])
             self.initialize()
-            else puts "number check  it\'s successful"
+            else puts "number check  is successful"
         end 
       end
-	  def pol
+	  def gender
 	puts "Введите ваш Пол 1-Муж или 0-Женщ "
 	@m_w=gets.to_i
 	if @m_w==1
@@ -39,13 +43,22 @@ class Client_Form
      puts "Данные Корректны! серия - #{@pos1} Номер - #{@pos2}"
      else self.passport
      end
-	 puts "Введите дату рождения"
-	 db=gets.chomp
-	 puts "ваша дата рождения #{db}"
+	 self.birth_date
+	end
+	def birth_date
+	puts "Введите дату Рождения,используя разделитель- ."
+	@data_born=gets.split('.')
+	@day=@data_born[0]
+	@month=@data_born[1]
+	@year=@data_born[2]
+	if @day[/^[1-31]/]&&@month[/^\d+$/]&&@year[/^\d+$/]
+	puts "Your birth date is #{@day}.#{@month}.#{@year}"
+	else self.birth_date
+	end
 	end
    end
    a=Client_Form.new()
-   a.pol
+   a.gender
    a.passport
    puts a.name.capitalize, a.sec_name.capitalize, a.last_name.capitalize
-   puts "Ваши Данные Успешно Внесены"
+   puts "Ваши Данные Успешно Внесены, приятного пребывания в отеле"
