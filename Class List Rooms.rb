5763746e3cbccc571a9fc7e4fbb5b8ba3983ea4e
Albert-Ros <albrol@mail.ru>
2020-05-08 01:04:27 +0300

    Добавлена возможность изменения статуса номера

--- Class List Rooms.rb
+++ Class List Rooms.rb
@@ -5,7 +5,7 @@ class List_Rooms
 def initialize(str)
 puts "Список Номеров"
 @rooms=str.split(',')
-puts @rooms[0],@rooms[1],@rooms[2]
+puts @rooms[0],@rooms[1],@rooms[2],"сейчас все номера свободны "
 end
   def show_description
 puts "введите цифру от 1 до 3 для просмотра описания номеров"
@@ -25,6 +25,19 @@ puts "Люкс номер с видом на пляж есть: 3 спальни
 вам предоставляеся безлимитный пропуск на все вечерние и пляжные развлечения,цена 5500р в сутки"
     end
   end
+  def status_change
+  puts "Введите порядковый номер комнаты  и статус,на которой хотите изменить: 
+  1-ordinary,2-middle-luxe,3-luxe 
+   статус: 1-свободен, 0-занят"
+  number=gets.to_i
+  status_room=gets.to_i
+  if status_room==1
+  puts "номер #{number} свободен"
+  else 
+  puts "номер #{number} занят"
+  end
+  end
 end
 a=List_Rooms.new("ordinary,middle-luxe,luxe")
 a.show_description
+a.status_change
