7e6fbef6058b00805a6371149e9f4564397d312f
Albert-Ros <albrol@mail.ru>
2020-05-14 00:23:50 +0300

    добавлено два метода add_room and delete_room

--- Class List Rooms.rb
+++ Class List Rooms.rb
@@ -1,43 +1,43 @@
 class List_Rooms
   attr_accessor :status_room
-  @description
   @rooms
 def initialize(str)
 puts "Список Номеров"
 @rooms=str.split(',')
 puts @rooms[0],@rooms[1],@rooms[2],"сейчас все номера свободны "
 end
-  def show_description
-puts "введите цифру от 1 до 3 для просмотра описания номеров"
-puts "1 - ordinary room, 2 - middle-luxe-room, 3 - luxe room"
-@description=gets.to_i
-  case(@description)
+  def show_description(description)
+  case(description)
     when(1)
 puts "Обычный однокомнатный номер с видом на пляж, есть: wi-fi,
-душевая кабина,телевизор, двух-спальная кравать завтрак включен,цена 1000р в сутки"
+душевая кабина,телевизор, двух-спальная, кровать, завтрак включен,цена 1000р в сутки"
     when(2)
 puts "Полу-Люкс номер с видом на пляж есть: две двух-спальных кравати,
-гостинная,кухня,mini-бар,шветский стол,Wi-Fi,ванная комната, цена 3500p в сутки"
+гостинная,кухня,mini-бар,шведский стол,Wi-Fi,ванная комната, цена 3500p в сутки"
     when(3)
 puts "Люкс номер с видом на пляж есть: 3 спальни,
-гостинная,кухня,mini-бар,детская,шветский стол,Wi-Fi,Джакузи,
-по желанию можно вызвать самилье в номер и попробовать различные вина,
-вам предоставляеся безлимитный пропуск на все вечерние и пляжные развлечения,цена 5500р в сутки"
+гостинная,кухня,mini-бар,детская комната,шведский стол,Wi-Fi,джакузи в номере,
+по желанию можно вызвать сомелье в номер и попробовать различные вина,
+вам предоставляется безлимитный пропуск на все вечерние и пляжные развлечения,цена 5500р в сутки"
     end
   end
-  def status_change
-  puts "Введите порядковый номер комнаты  и статус,на которой хотите изменить: 
-  1-ordinary,2-middle-luxe,3-luxe 
-   статус: 1-свободен, 0-занят"
-  number=gets.to_i
-  status_room=gets.to_i
+  def status_change(number,status_room)
   if status_room==1
   puts "номер #{number} свободен"
   else 
   puts "номер #{number} занят"
   end
   end
+  def add_room(elem)
+  @rooms.push(elem)
+  puts @rooms
+  end
+  def delete_room(pat)
+  @rooms.delete(pat)
+  puts @rooms
+  end
 end
-a=List_Rooms.new("ordinary,middle-luxe,luxe")
-a.show_description
-a.status_change
+
+
+
+
