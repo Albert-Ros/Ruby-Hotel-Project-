63a30f61fe005d2e70b509683447a698b518b74d
Albert-Ros <albrol@mail.ru>
2020-05-20 00:08:57 +0300

    Добавлена несколько кнопок

--- Admin GUI.rb
+++ Admin GUI.rb
@@ -9,20 +9,19 @@ def initialize(app)
 super(app, "HOTEL ELEON administration" , :width => 600, :height => 400)
 photo = Photo_hotel.new("ordinary.jpg" )
 photo_view = PhotoView.new(self, photo)
-@button_two= FXButton.new(self,"Description Rooms")
-@button_one= FXButton.new(self,"Exit")
-@button_two.connect(SEL_COMMAND,method(:show_Des))
-@button_one.connect(SEL_COMMAND,method(:onclick))
-simple = FXTextField.new(self, 19, nil, 10,
- JUSTIFY_CENTER_X|FRAME_SUNKEN|
- FRAME_THICK|LAYOUT_SIDE_TOP)
- simple.text = "введите ФИО клиента "
+@button_one= FXButton.new(self,"Description Rooms")
+@button_three=FXButton.new(self,"Add client")
+@button_for=FXButton.new(self,"Delete Client")
+@button_five=FXButton.new(self,"show rooms")
+@button_two= FXButton.new(self,"Exit")
+@button_one.connect(SEL_COMMAND,method(:show_Des))
+@button_two.connect(SEL_COMMAND,method(:Exit))
 end
 def create
 super
 show(PLACEMENT_SCREEN)
 end
-def onclick(sender,sel,ptr)
+def Exit(sender,sel,ptr)
 app.exit
 end
 def show_Des(sender,sel,ptr)
