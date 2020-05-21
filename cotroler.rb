fb5a357ff297a572fcf31f470769fa5dd7151ef1
Albert-Ros <albrol@mail.ru>
2020-05-21 03:03:46 +0300

    Controler MVC

---
+++ cotroler.rb
@@ -0,0 +1,10 @@
+current_path = File.dirname(__FILE__)
+require "#{current_path}/CLASS List Rooms"
+require "#{current_path}/Admin GUI.rb"
+require "#{current_path}/all tables hotel.rb"
+require 'fox16'
+include Fox
+class Controller_MVC
+def 
+
+end 
