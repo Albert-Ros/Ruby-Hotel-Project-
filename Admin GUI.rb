833409fccb8b69600a9b0aed7e4a4a459327fec7
Albert-Ros <albrol@mail.ru>
2020-06-05 01:13:28 +0300

    Update Admin GUI.rb

--- Admin GUI.rb
+++ Admin GUI.rb
@@ -1,35 +1,39 @@
 require 'fox16'
 include Fox
 current_path = File.dirname(__FILE__)
-require "#{current_path}/class photo hotel.rb"
-require "#{current_path}/Class photo view.rb"
-require "#{current_path}/Class List Rooms.rb"
 require "#{current_path}/controler.rb"
 class HOTEL_Admin < FXMainWindow
+@mvc=Controller_MVC.new
 def initialize(app)
 super(app, "HOTEL ELEON administration" , :width => 600, :height => 400)
 photo = Photo_hotel.new("ordinary.jpg" )
 photo_view = PhotoView.new(self, photo)
-@button_one= FXButton.new(self,"Description Rooms")
-@button_three=FXButton.new(self,"Add client")
-@button_for=FXButton.new(self,"Delete Client")
-@button_five=FXButton.new(self,"show rooms")
-@button_two= FXButton.new(self,"Exit")
-@button_one.connect(SEL_COMMAND,method(:show_Des))
-@button_two.connect(SEL_COMMAND,method(:Exit))
+@button_one= FXButton.new(self,"show clients")
+@button_two=FXButton.new(self,"show rooms")
+@button_trhee=FXButton.new(self,"show Booking")
+@button_for= FXButton.new(self,"Exit")
+@button_one.connect(SEL_COMMAND,method(:show_clients))
+@button_two.connect(SEL_COMMAND,method(:show_rooms))
+@button_trhee.connect(SEL_COMMAND,method(:show_booking))
+@button_for.connect(SEL_COMMAND,method(:exit))
 end
 def create
 super
 show(PLACEMENT_SCREEN)
 end
+def show_clients(sender,sel,ptr)
+@mvc.show_clients
+end
+def show_rooms(sender,sel,ptr)
+@mvc.show_rooms
+end
+def show_booking(sender,sel,ptr)
+@mvc.show_booking
+end
 def Exit(sender,sel,ptr)
 app.exit
 end
-def show_Des(sender,sel,ptr)
-
 end
-end
-
 if __FILE__ == $0
 FXApp.new do |app|
 HOTEL_Admin.new(app)
